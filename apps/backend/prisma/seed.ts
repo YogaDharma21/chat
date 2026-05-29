f0cf5165510e3329f734560db309776ac172fdc3
YogaDharma21 <yogacode2006@gmail.com>
2026-05-29 21:26:42 +0800

    feat(backend): add prisma seed script

---
+++ apps/backend/prisma/seed.ts
@@ -0,0 +1,41 @@
+import "dotenv/config";
+import { PrismaPg } from "@prisma/adapter-pg";
+import { PrismaClient, RoleType } from "../src/generated/prisma/client";
+
+const connectionString = `${process.env.DATABASE_URL}`;
+const adapter = new PrismaPg({ connectionString });
+const prisma = new PrismaClient({ adapter });
+
+async function main() {
+    const roles: RoleType[] = ["ADMIN", "MEMBER", "OWNER", "USER"];
+
+    for (const role of roles) {
+        const roleExist = await prisma.role.findFirst({
+            where: {
+                role: role,
+            },
+        });
+
+        await prisma.role.upsert({
+            where: {
+                id: roleExist?.id ?? "",
+            },
+            create: {
+                role: role,
+            },
+            update: {},
+        });
+    }
+
+    console.log("Success seeding roles");
+}
+
+main()
+    .then(async () => {
+        await prisma.$disconnect();
+    })
+    .catch(async (err) => {
+        console.error(err);
+        await prisma.$disconnect();
+        process.exit(1);
+    });
